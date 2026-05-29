bfb99a6947efe377bf09f052c9d9df6257e3cbb5
Student <Student@LabVM2049826>
2026-05-29 15:15:16 +0200

    Add project files.

---
+++ CyberBot/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CyberBot
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            new SoundGreet() { };
+        }
+
+        private void submit_name(object sender, RoutedEventArgs e)
+        {
+            username_grid.Visibility = Visibility.Hidden;
+
+            background_grid.Visibility = Visibility.Visible;
+        }
+
+        private void sendButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CyberBot/SoundGreet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberBot
+{
+    public class SoundGreet
+    {
+
+        public SoundGreet()
+        {//start of constructor
+
+            string auto_path = AppDomain.CurrentDomain.BaseDirectory.Replace(@"\bin\Debug\net8.0-windows\", @"\greet.wav");
+
+            //create an instance for the SoundPlayer class
+            SoundPlayer greetMe = new SoundPlayer(auto_path);
+
+            //then greet 
+            greetMe.Play();
+        }
+    }
+}
